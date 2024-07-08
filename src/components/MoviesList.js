1723bbf84045cdb214b7302eca9d2236791dd130
Vishesh <thakralvishesh@gmail.com>
2024-07-08 18:27:51 +0530

    Added carousel to movie lists

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,17 +1,26 @@
 import React from "react";
 import MovieCard from "./MovieCard";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const MoviesList = ({ title, movies }) => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 6,
+    slidesToScroll: 5,
+  };
+
   return (
-    <div className="px-6">
+    <div className="px-12 mt-[36px]">
       <h1 className="text-3xl py-4 text-white">{title}</h1>
-      <div className="flex overflow-x-scroll">
-        <div className="flex">
-          {movies?.map((movie) => (
-            <MovieCard key={movie.id} posterPath={movie.poster_path} />
-          ))}
-        </div>
-      </div>
+      <Slider {...settings}>
+        {movies?.map((movie) => (
+          <MovieCard key={movie.id} posterPath={movie.poster_path} />
+        ))}
+      </Slider>
     </div>
   );
 };
